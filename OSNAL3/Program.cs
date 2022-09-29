13e4d283e33446eb07243f913724cf3096d454e8
Huawei <Huawei@LAPTOP-5LRHA01L>
2022-09-29 14:41:25 +0300

    Добавьте файлы проекта.

---
+++ OSNAL3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        double A, B, H;
+        Console.WriteLine("Минимальное число:");
+        A = int.Parse(Console.ReadLine());
+        Console.WriteLine("Максимально число:");
+        B = int.Parse(Console.ReadLine());
+        Console.WriteLine("Нужное число сна:");
+        H = int.Parse(Console.ReadLine());
+        if (A < B)
+        {
+            if (H < A)
+            {
+                Console.WriteLine("Недосып");
+            }
+             else if (H > B)
+            {
+                Console.WriteLine("Пересып");
+            }
+             else
+            {
+                Console.WriteLine("Это нормально");
+            }
+
+            
+        }
+        else
+        {
+            Console.WriteLine("Данные неверные");
+        }
+        
+    }
+
+}
+
